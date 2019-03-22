fe48de8dcd4051982557ea21dd3a9868e0510eae
PhilipeTI <34010944+PhilipeTI@users.noreply.github.com>
2019-03-22 19:07:29 -0300

    Add files via upload

---
+++ questão 03/js.js
@@ -0,0 +1,42 @@
+<head>
+	<script language="Javascript">
+	function skin1()
+	{
+	document.Cor1='red';
+	}
+	function skin2()
+	{
+	document.Cor2='green';
+	}
+	function skin3()
+	{
+	document.Cor3='blue';
+	}
+	function skin4()
+	{
+	document.Cor4='yellow';
+	}
+
+	</script>
+</head>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
